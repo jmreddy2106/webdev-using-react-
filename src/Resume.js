d160778cf82e75c29c988ed013d539cc76cc0825
unknown <jmreddy@lbrce.ac.in>
2020-08-08 16:45:20 +0530

    link route

--- src/Resume.js
+++ src/Resume.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import {useLocation} from 'react-router-dom';
+import data from './data/data.json';
 
-let Resume = (props) => {
+let Resume = () => {
+    let uselocation = {useLocation};
+    let arrayIndex = uselocation.info.id;
+    let singleProfile = data.profile[arrayIndex];
 
     return (
-
         <div>
-            
+            <div>
+                <h2>
+                    {singleProfile.basic.name}
+                </h2>
+            </div>
 
+            <div>
+            <h2>
+                {singleProfile.skills.map((skillval, skillIdx) => (
+
+                    <div key={skillIdx}>
+                        <h3>{skillval.type}</h3>
+                        {singleProfile.skills.map((dataVal, valIdx) => (
+                            <span key={valIdx}> 
+                                {dataVal.values}
+                            </span>
+                        ))}
+                    </div>
+                )
+                
+                )
+
+                }
+            </h2>
+
+            </div>
         </div>
-
     );
 
 }
